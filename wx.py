dbf719f91a25d0face3e68abb6b8eb05e6ef4c89
lazygunner <gymgunner@gmail.com>
2013-11-06 09:57:43 +0800

    init enviroment

---
+++ wx.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from flask import Flask
+from flask.ext.mongoengine import MongoEngine
+
+app = Flask(__name__)
+app.config["MONGODB_SETTINGS"] = {'DB':"wx"}
+app.config["SECRET_KEY"] = "kalashinikov"
+
+db = MongoEngine(app)
+
+#def register_blueprints(app):
+
+#register_blueprints(app)
+
+
+app.debug=True
+
+if __name__ == '__main__':
+    app.run()
+
+
